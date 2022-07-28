b62c908d85b9fdb187a3c6897981da390e42270b
Bruno  Henrique Brandl <74260274+bruno21637@users.noreply.github.com>
2022-07-28 15:17:22 -0300

    Iniciando o Layout do desafio

---
+++ dasafio07/src/components/MovieHead.vue
@@ -0,0 +1,50 @@
+<template>
+     <div class="container">
+    <div class="container-text">
+       <h1>Bem Vindo(a) ao Cine Movie</h1>
+      <p>Aqui você Pode Adicionar ou Assistir Filmes </p>
+    </div>
+    <div>
+   <router-link to="/register"> <button class="btn">Cadastrar Novo Filme</button></router-link>
+    </div>
+  </div>
+</template>
+<script>
+
+</script>
+<style scoped>
+.container {
+  width: 100%;
+  padding: 58px;
+  margin-top: 3vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  color: white;
+  background-color: #6c6a6b;
+}
+h1 {
+  font-size: 50px;
+
+}
+.btn {
+ border: solid 2px rgb(230, 230, 230);
+  background-color: rgb(152, 152, 152);
+  color: rgb(0, 0, 0);
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 20px;
+  margin: 16px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+  border-radius: 11px;
+  height: 82px;
+  width: 305px;
+}
+.btn:hover {
+  background-color: black;
+  color: rgb(255, 255, 255);
+}
+</style> 
---
+++ dasafio07/src/views/MovieList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <navList />
+    <MovieHead />
+  </div>
+</template>
+<script>
+import navList from "../components/navList.vue"
+import MovieHead from "../components/MovieHead.vue"
+export default {
+  components: {
+    navList,
+    MovieHead,
+  },
+
+};
+</script>
+<style scoped>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  color: white;
+  font-family: Arial, Helvetica, sans-serif;
+  background-color: #6c6a6b;
+
+}
+</style>
